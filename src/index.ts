f8a6af160681ba65632f5b94e82468c9fbc39fcc
wjq <1092229612@qq.com>
2024-01-12 23:15:40 +0800

    feat: 优化项目自恢复能力

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,50 @@ import fileRoutes from './routers/file'
 import productRoutes from './routers/product'
 import customerRoutes from './routers/customer'
 
-AppDataSource.initialize()
-  .then(async () => {
-    // 创建和设置Express应用程序
-    const app = express()
-    app.use(cors())
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true })) // 解析表单数据
+function initializeAppDataSource() {
+  return AppDataSource.initialize()
+    .then(async () => {
+      // 创建和设置Express应用程序
+      const app = express()
+      app.use(cors())
+      app.use(express.json())
+      app.use(express.urlencoded({ extended: true })) // 解析表单数据
 
-    app.use('/user', userRoutes)
-    app.use('/order', orderRoutes)
-    app.use('/file', fileRoutes)
-    app.use('/product', productRoutes)
-    app.use('/customer', customerRoutes)
+      app.use('/user', userRoutes)
+      app.use('/order', orderRoutes)
+      app.use('/file', fileRoutes)
+      app.use('/product', productRoutes)
+      app.use('/customer', customerRoutes)
 
-    // 启动Express服务器
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000')
+      let server
+
+      function startServer() {
+        server = app.listen(3000, () => {
+          console.log('Server is running on port 3000')
+        })
+      }
+
+      function stopServer() {
+        if (server) {
+          server.close(() => {
+            console.log('Server is stopped')
+          })
+        }
+      }
+      // 启动Express服务器
+      startServer()
+      // Handle uncaught exceptions
+      process.on('uncaughtException', (err) => {
+        stopServer()
+        startServer()
+      })
     })
-  })
-  .catch((error) => console.log(error))
+    .catch((error) => {
+      console.log(error)
+      setTimeout(() => {
+        initializeAppDataSource()
+      }, 1000)
+    })
+}
+
+initializeAppDataSource()
